ae2cfff39094221f34730f9bcc6bac5f6384cabf
Sumit <sumit13thakur124@gmail.com>
2025-12-03 14:25:47 +0530

    Add : Added Validator middleware

---
+++ src/validators/index.js
@@ -0,0 +1,32 @@
+import {body} from "express-validator";
+
+const userRegistrationValidators = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email field cannot be empty.")
+            .isEmail()
+            .withMessage("Please provide a valid email address."),
+        
+        body("userName")
+            .trim()
+            .notEmpty()
+            .withMessage("Username field cannot be empty.") 
+            .isLowercase()
+            .withMessage("Username must be in lowercase.")
+            .isLength({ min: 3, max: 30 })  
+            .withMessage("Username must be between 3 and 30 characters long."),   
+
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password field cannot be empty.")
+            .isLength({ min: 8 })
+            .withMessage("Password must be at least 8 characters long.")
+
+    ]
+
+}
+
+export { userRegistrationValidators };
